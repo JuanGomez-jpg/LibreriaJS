2ae909f66e0d38e7ad0d45c9f45ac1e2f005ab0a
Juan M <albertomojarro32@gmail.com>
2021-04-09 19:14:15 -0500

    Version 2 de proceder a comprar libros, ya muestra el costo del envio, ya sea rapido o normal

--- JS/checkOut.js
+++ JS/checkOut.js
@@ -1,18 +1,41 @@
 
 window.onload = obtenerLibrosCarrito;
 const bookDOM = document.querySelector('.product__center');
+const subTotalDOM = document.querySelector('.subtotal__books');
+
 
 var libros = [];
+let subtotal = 0.0;
+let precio = 0.0;
+let cantidad = 0;
+let total = 0.0;
+tipo = true;
 
 function VolverInicio () {
     location.href ="http://localhost/node-libreria/";
 } 
 
+function setPrecioEnviarN () {
+  console.log("Normal");
+  tipo = true;
+  displaySubTotal();
+}
+
+function setPrecioEnviarR () {
+  console.log("Rapido");
+  tipo = false;
+  displaySubTotal();
+}
+
+
 function displayProducts(obj){
     let results = '';
     let img = '.';
+    total = 0.0;
     obj.forEach(({title,image,id,price, amount}) => {
-    let subtotal = 0.0;
+    subtotal = 0.0;
+    precio = parseFloat(price, 2);
+    cantidad = parseInt(amount);
     subtotal = parseFloat(price, 2) * parseInt(amount);  
     img = '.';
     img += image;
@@ -28,15 +51,49 @@ function displayProducts(obj){
                         Cantidad: ${amount}
                         </br>
                         </br>
-                        Subtotal: $${subtotal}
+                        Total: $${subtotal}
                         </div>
                       </div>
                     </div>
                   </div>`;
+      total += parseFloat(price, 2) * parseInt(amount);  
     });
 
+    let iva, totalCE;
+    iva = 0.0;
+    totalCE = 0.0;
+    iva = total * .16;
+    total += iva;
+    total = parseFloat(total.toFixed(2));
+
     bookDOM.innerHTML = results;
+
+    displaySubTotal();
   }
+
+  function displaySubTotal(){
+    let tipoE = 0.00;
+    if (tipo) {
+      totalCE = total + 20.00;
+      tipoE = 20.00;
+    } else {
+      totalCE = total + 100.00
+      tipoE = 100.00;
+    }
+    let results = '';
+    results = `<div>
+        <br>
+        Subtotal: $${total}
+        <br>
+        Envío: $${tipoE}
+        <br>
+        Total: $${totalCE}
+
+        </div>`;
+
+    subTotalDOM.innerHTML = results;
+  }
+
 
 function obtenerLibrosCarrito () {
     libros = localStorage.getItem("cart")
