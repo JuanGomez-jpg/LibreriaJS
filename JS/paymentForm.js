571ae9e8f0abebd7613ad839d3a48b6f7aecf731
Juan M <albertomojarro32@gmail.com>
2021-04-12 00:15:01 -0500

    Adelanto de reducri el stock de cada libro

--- JS/paymentForm.js
+++ JS/paymentForm.js
@@ -10,14 +10,68 @@ function addAll(){
     ? JSON.parse(localStorage.getItem("cart"))
     : [];
 
+    /* Algoritmo para obtener la cantidad de libros a comprar por el cliente y su id de cada libro */
+    let librosDelCarrito = [];
+    let cantidadLibroCompra;
+    let idLibroCompra;
+    for (let i = 0 ; i < allCart.length ; ++i) {
+      cantidadLibroCompra = allCart[i].cantidad;
+      idLibroCompra = allCart[i].eventId;
+      var newBookCart = {
+        cantidad: cantidadLibroCompra,
+        eventId: idLibroCompra
+      };
+      librosDelCarrito.push(newBookCart);
+    }
+    console.log(librosDelCarrito);
+
+    /* Algoritmo para obtener todos los libros disponibles para despues */
+    /* compararlos y hacer la actualización de reducir la cantidad de libros
+    /* disponibles en la base de datos */
+    let allBooks = JSON.parse(localStorage.getItem("products"));
+    allBooks = localStorage.getItem("products")
+    ? JSON.parse(localStorage.getItem("products"))
+    : [];
+    console.log(allBooks);
+
+    var keys = Object.keys(allBooks);
+
+    for(let i =0 ; i < keys.length; ++i) {
+      var currentObj = allBooks[keys[i]];
+
+      if (librosDelCarrito[i].eventId == currentObj.eventId) {
+        console.log("Si jala");
+        break;
+      }
+
+    }
+/*
+    var newBookUpdated = {
+      eventId: postKey,
+      tittle: tituloValue,
+      autor: autorValue,
+      anio: anioValue,
+      categoria: categoriaValue,
+      subgenero: subgeneroValue,
+      editorial: editorialValue,
+      cantidad: cantValue,
+      precio: precioValue,
+      isbn: isbnValue,
+      descripcion: descripcionValue,
+      url: downloadURL,
+      id: id
+  };*/
+
+
+
     let active = JSON.parse(localStorage.getItem("activeUser"));
     active = localStorage.getItem("activeUser")
     ? JSON.parse(localStorage.getItem("activeUser"))
     : [];
 
-    console.log(allCart);
+    //console.log(allCart);
 
-    let noTarjeta = document.getElementById("noTarjeta").value;
+   /* let noTarjeta = document.getElementById("noTarjeta").value;
     let nombreUsuario = active.username;
     let direccion = active.address;
     let idUser = active.eventId;
@@ -53,9 +107,7 @@ function addAll(){
     let checkOutP = [checkOut,pedido,librosP];
 
     updates[`/checkOut/ ${idUser}`] = checkOutP;
-    firebase.database().ref().update(updates);
-
-
+    firebase.database().ref().update(updates);*/
 
 }
 
